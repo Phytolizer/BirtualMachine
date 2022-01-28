32a06c9b73d73b3215c7c80b55ca161be574da34
Kyle Coffey <me@phytolizer.dev>
2022-01-28 09:04:23 -0600

    Create a basic framework for stack manipulation.

--- bm.cpp
+++ bm.cpp
@@ -1,5 +1,140 @@
+#include <array>
 #include <iostream>
+#include <span>
 
-int main() {
-	std::cout << "hello, world\n";
+#define TRAPS_X                                                                                                        \
+	X(Ok)                                                                                                              \
+	X(StackOverflow)                                                                                                   \
+	X(StackUnderflow)                                                                                                  \
+	X(IllegalInstruction)
+
+struct IllegalTrapException final : std::runtime_error
+{
+	IllegalTrapException() : runtime_error("Encountered an illegal Trap value.")
+	{
+	}
+};
+
+enum struct Trap
+{
+#define X(x) x,
+	TRAPS_X
+#undef X
+};
+
+static constexpr const char* trapAsCstr(const Trap trap)
+{
+	switch (trap)
+	{
+#define X(x)                                                                                                           \
+	case Trap::x:                                                                                                      \
+		return #x;
+		TRAPS_X
+#undef X
+		default:
+			throw IllegalTrapException{};
+	}
+}
+
+using Word = std::int64_t;
+
+constexpr std::size_t STACK_CAPACITY = 1024;
+
+enum struct InstructionType
+{
+	Push,
+	Plus,
+};
+
+struct Instruction
+{
+	InstructionType type;
+	Word operand;
+
+	static constexpr Instruction push(Word operand)
+	{
+		return {
+		    .type = InstructionType::Push,
+		    .operand = operand,
+		};
+	}
+
+	static constexpr Instruction plus()
+	{
+		return {
+		    .type = InstructionType::Plus,
+		};
+	}
+};
+
+struct Bm
+{
+	std::array<Word, STACK_CAPACITY> stack;
+	std::size_t stackSize;
+
+	Trap executeInstruction(const Instruction inst)
+	{
+		switch (inst.type)
+		{
+			case InstructionType::Push:
+				if (stackSize >= STACK_CAPACITY)
+				{
+					return Trap::StackOverflow;
+				}
+				stack[stackSize++] = inst.operand;
+				break;
+			case InstructionType::Plus:
+				if (stackSize < 2)
+				{
+					return Trap::StackUnderflow;
+				}
+				stack[stackSize - 2] += stack[stackSize - 1];
+				stackSize -= 1;
+				break;
+			default:
+				return Trap::IllegalInstruction;
+		}
+
+		return Trap::Ok;
+	}
+
+	void dump(std::ostream& os) const
+	{
+		os << "Stack:\n";
+		if (stackSize > 0)
+		{
+			for (std::size_t i = 0; i < stackSize; ++i)
+			{
+				os << "  " << stack[i] << "\n";
+			}
+		}
+		else
+		{
+			os << "  [empty]\n";
+		}
+	}
+};
+
+Bm bm{};
+constexpr std::array PROGRAM = {
+    Instruction::push(69),
+    Instruction::push(420),
+    Instruction::plus(),
+};
+
+int main(const int argc, char** argv)
+{
+	std::span args{argv, static_cast<std::size_t>(argc)};
+
+	bm.dump(std::cout);
+	for (const Instruction& inst : PROGRAM)
+	{
+		if (const Trap trap = bm.executeInstruction(inst); trap != Trap::Ok)
+		{
+			std::cerr << "Trap activated: " << trapAsCstr(trap) << "\n";
+			bm.dump(std::cerr);
+			return 1;
+		}
+	}
+	bm.dump(std::cout);
 }
