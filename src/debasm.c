7055d482bbd8317a1e30137ed660c35543fe8fab
Kyle Coffey <me@phytolizer.dev>
2022-04-25 19:39:05 -0500

    disassembler

--- src/debasm.c
+++ src/debasm.c
@@ -10,7 +10,48 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
+	const char* input_file_path = argv[1];
+
+	bm_load_program_from_file(&bm, input_file_path);
+
 	for (size_t i = 0; i < bm.program_size; i++) {
-		switch (bm.program[i].type) {}
+		switch (bm.program[i].type) {
+			case inst_type_nop:
+				printf("nop\n");
+				break;
+			case inst_type_push:
+				printf("push %" PRI_WORD "\n", bm.program[i].operand);
+				break;
+			case inst_type_plus:
+				printf("plus\n");
+				break;
+			case inst_type_minus:
+				printf("minus\n");
+				break;
+			case inst_type_mult:
+				printf("mult\n");
+				break;
+			case inst_type_div:
+				printf("div\n");
+				break;
+			case inst_type_jump:
+				printf("jmp %" PRI_WORD "\n", bm.program[i].operand);
+				break;
+			case inst_type_jump_if:
+				printf("jmp_if %" PRI_WORD "\n", bm.program[i].operand);
+				break;
+			case inst_type_eq:
+				printf("eq\n");
+				break;
+			case inst_type_halt:
+				printf("halt\n");
+				break;
+			case inst_type_print_debug:
+				printf("print_debug\n");
+				break;
+			case inst_type_dup:
+				printf("dup %" PRI_WORD "\n", bm.program[i].operand);
+				break;
+		}
 	}
 }
